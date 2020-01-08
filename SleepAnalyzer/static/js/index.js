1cc22babc5c0ffc076931c4e1f072f3de284409f
Danielle Ali <danielleali@Danielles-Mac-mini.local>
2020-01-07 23:15:34 -0500

    Added link to css in index.html and modified setting.py with STATICFILES_DIRS

---
+++ SleepAnalyzer/static/js/index.js
@@ -0,0 +1,6 @@
+
+const userAction = async () => {
+  const response = await fetch('http://example.com/movies.json');
+  const myJson = await response.json(); //extract JSON from the http response
+  // do something with myJson
+}
